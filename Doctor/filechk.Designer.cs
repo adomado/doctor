3eadde1adaf17580812a889989219cf978d239b6
makuchaku <makuchaku@adomado.com>
2010-12-28 11:56:32 +0530

    Initial import of code

---
+++ Doctor/filechk.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Doctor
+{
+    public partial class filechk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(filechk));
+            this.uninstallerTest_btn = new System.Windows.Forms.Button();
+            this.err_list = new System.Windows.Forms.ListBox();
+            this.Registry_Values_lbl = new System.Windows.Forms.Label();
+            this.load_text = new System.Windows.Forms.Label();
+            this.list_head_lbl = new System.Windows.Forms.Label();
+            this.messages = new System.Windows.Forms.ListBox();
+            this.installerTest_btn = new System.Windows.Forms.Button();
+            this.message_lbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Reg_lbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uninstallerTest_btn
+            // 
+            this.uninstallerTest_btn.BackColor = System.Drawing.Color.Maroon;
+            this.uninstallerTest_btn.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.uninstallerTest_btn.Location = new System.Drawing.Point(851, 427);
+            this.uninstallerTest_btn.Name = "uninstallerTest_btn";
+            this.uninstallerTest_btn.Size = new System.Drawing.Size(116, 49);
+            this.uninstallerTest_btn.TabIndex = 2;
+            this.uninstallerTest_btn.Text = "UNINSTALLER TEST";
+            this.uninstallerTest_btn.UseVisualStyleBackColor = false;
+            this.uninstallerTest_btn.Click += new System.EventHandler(this.uninstallerTest_btn_Click);
+            // 
+            // err_list
+            // 
+            this.err_list.BackColor = System.Drawing.Color.Snow;
+            this.err_list.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.err_list.FormattingEnabled = true;
+            this.err_list.Location = new System.Drawing.Point(3, 121);
+            this.err_list.Name = "err_list";
+            this.err_list.Size = new System.Drawing.Size(281, 364);
+            this.err_list.TabIndex = 3;
+            this.err_list.Visible = false;
+            // 
+            // Registry_Values_lbl
+            // 
+            this.Registry_Values_lbl.AutoSize = true;
+            this.Registry_Values_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registry_Values_lbl.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Registry_Values_lbl.Location = new System.Drawing.Point(342, 121);
+            this.Registry_Values_lbl.Name = "Registry_Values_lbl";
+            this.Registry_Values_lbl.Size = new System.Drawing.Size(83, 13);
+            this.Registry_Values_lbl.TabIndex = 5;
+            this.Registry_Values_lbl.Text = "Registry Values ";
+            this.Registry_Values_lbl.Visible = false;
+            // 
+            // load_text
+            // 
+            this.load_text.AutoSize = true;
+            this.load_text.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.load_text.Location = new System.Drawing.Point(80, 222);
+            this.load_text.Name = "load_text";
+            this.load_text.Size = new System.Drawing.Size(836, 120);
+            this.load_text.TabIndex = 7;
+            this.load_text.Text = "Problem after installtion/uninstallation? \n                 Don\'t worry, Choose a" +
+                " test:";
+            // 
+            // list_head_lbl
+            // 
+            this.list_head_lbl.AutoSize = true;
+            this.list_head_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list_head_lbl.ForeColor = System.Drawing.Color.DarkRed;
+            this.list_head_lbl.Location = new System.Drawing.Point(0, 78);
+            this.list_head_lbl.Name = "list_head_lbl";
+            this.list_head_lbl.Size = new System.Drawing.Size(125, 25);
+            this.list_head_lbl.TabIndex = 8;
+            this.list_head_lbl.Text = "Missing Files";
+            this.list_head_lbl.Visible = false;
+            // 
+            // messages
+            // 
+            this.messages.BackColor = System.Drawing.Color.Snow;
+            this.messages.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.messages.FormattingEnabled = true;
+            this.messages.Location = new System.Drawing.Point(784, 121);
+            this.messages.Name = "messages";
+            this.messages.Size = new System.Drawing.Size(216, 208);
+            this.messages.TabIndex = 9;
+            this.messages.Visible = false;
+            // 
+            // installerTest_btn
+            // 
+            this.installerTest_btn.BackColor = System.Drawing.Color.Maroon;
+            this.installerTest_btn.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.installerTest_btn.Location = new System.Drawing.Point(851, 345);
+            this.installerTest_btn.Name = "installerTest_btn";
+            this.installerTest_btn.Size = new System.Drawing.Size(116, 49);
+            this.installerTest_btn.TabIndex = 1;
+            this.installerTest_btn.Text = "INSTALLER TEST";
+            this.installerTest_btn.UseVisualStyleBackColor = false;
+            this.installerTest_btn.Click += new System.EventHandler(this.installerTest_btn_Click);
+            // 
+            // message_lbl
+            // 
+            this.message_lbl.AutoSize = true;
+            this.message_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.message_lbl.ForeColor = System.Drawing.Color.DarkRed;
+            this.message_lbl.Location = new System.Drawing.Point(779, 78);
+            this.message_lbl.Name = "message_lbl";
+            this.message_lbl.Size = new System.Drawing.Size(103, 25);
+            this.message_lbl.TabIndex = 10;
+            this.message_lbl.Text = "Messages";
+            this.message_lbl.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::ChkTest.Properties.Resources.AdoMado;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1015, 64);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Reg_lbl
+            // 
+            this.Reg_lbl.AutoSize = true;
+            this.Reg_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reg_lbl.ForeColor = System.Drawing.Color.DarkRed;
+            this.Reg_lbl.Location = new System.Drawing.Point(340, 78);
+            this.Reg_lbl.Name = "Reg_lbl";
+            this.Reg_lbl.Size = new System.Drawing.Size(148, 25);
+            this.Reg_lbl.TabIndex = 11;
+            this.Reg_lbl.Text = "Registry Values";
+            this.Reg_lbl.Visible = false;
+            // 
+            // filechk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(1012, 488);
+            this.Controls.Add(this.Reg_lbl);
+            this.Controls.Add(this.message_lbl);
+            this.Controls.Add(this.messages);
+            this.Controls.Add(this.list_head_lbl);
+            this.Controls.Add(this.load_text);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Registry_Values_lbl);
+            this.Controls.Add(this.err_list);
+            this.Controls.Add(this.uninstallerTest_btn);
+            this.Controls.Add(this.installerTest_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "filechk";
+            this.Text = "AdoMado Doctor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button installerTest_btn;
+        private System.Windows.Forms.Button uninstallerTest_btn;
+        public System.Windows.Forms.ListBox err_list;
+        private System.Windows.Forms.Label Registry_Values_lbl;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label load_text;
+        private System.Windows.Forms.Label list_head_lbl;
+        private System.Windows.Forms.ListBox messages;
+        private System.Windows.Forms.Label message_lbl;
+        private System.Windows.Forms.Label Reg_lbl;
+    }
+}
+
---
+++ Doctor/filechk.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Doctor
+{
+    public partial class filechk : Form
+    {
+        public filechk()
+        {
+            InitializeComponent();
+        }
+
+        private void installerTest_btn_Click(object sender, EventArgs e)
+        {
+            err_list.Items.Clear();
+            messages.Items.Clear();
+            load_text.Visible = false;
+            Registry_Values_lbl.Text = "";
+            string[] Prog_files = { "PartnerToolbar.config", "AdomadoBar_1.0.2.dll", "AdoMadoBarUser.config", "app.config", "Interop.SHDocVw.dll", "Microsoft.mshtml.dll", "Newtonsoft.Json.Net20.dll", "RegisterAdoMado.exe", "SpicIE.dll", "UnInstallAdoMado.exe", "UnInstaller.exe", "wait.html", "Chrome\\Adomado.crx", "IE\\config.txt", "Updater\\AdomadoUpdater.exe", "Firefox\\install.rdf" };
+            string[] reg = {"AdoMadoApiKey","AdomadoToolbarId","CurrentUser","CurrentVersion","Guid" };
+            installer_tests init = new installer_tests();
+            string p = init.program_files(err_list,Prog_files,messages);
+            string r = init.registry_chk(err_list,reg,Registry_Values_lbl,messages);
+            string b = init.browser_chk(Registry_Values_lbl,messages);
+            
+            if ((p == "Success")&&(r == "Success")&&(b=="Success"))
+            {
+                Registry_Values_lbl.Visible = true;
+                messages.Visible = true;
+                message_lbl.Visible = true;
+                Registry_Values_lbl.Visible = true;
+                Reg_lbl.Visible = true;
+                MessageBox.Show("Installer Test SUCCESSFUL!");
+                messages.Items.Insert(0, "Installer Test SUCCESSFUL!");
+                                
+            }
+            else
+            {
+                Registry_Values_lbl.Visible = true;
+                list_head_lbl.Visible = true;
+                messages.Visible = true;
+                message_lbl.Visible = true;
+                Reg_lbl.Visible = true;
+                MessageBox.Show("Installer Test complete.\nRESULT : Some files/entries missing");
+                MessageBox.Show("Check the list for missing files/entries");
+                err_list.Visible = true;
+            } 
+        }
+
+        private void uninstallerTest_btn_Click(object sender, EventArgs e)
+        {
+            err_list.Items.Clear();
+            messages.Items.Clear();
+            load_text.Visible = false;
+            Registry_Values_lbl.Text = "";
+            string[] reg = { "AdoMadoApiKey", "AdomadoToolbarId", "CurrentUser", "CurrentVersion", "Guid" };
+            uninstaller_tests init = new uninstaller_tests();
+            installer_tests init1 = new installer_tests(); //same function call as installer just to read the registry
+            string p = init.program_files(err_list,messages);
+            string r = init1.registry_chk(err_list, reg, Registry_Values_lbl,messages);
+            string b = init1.browser_chk(Registry_Values_lbl, messages);
+            if ((p == "Success") && (r == "Fail")&& (b=="Fail"))
+            {
+                list_head_lbl.Visible = false;
+                messages.Visible = true;
+                message_lbl.Visible = true;
+                Reg_lbl.Visible = true;
+                messages.Items.Insert(0, "Uninstaller Test SUCCESSFUL!");
+                MessageBox.Show("Uninstaller Test SUCCESSFUL!");
+                
+            }
+            else
+            {
+                Registry_Values_lbl.Visible = true;
+                list_head_lbl.Visible = true;
+                message_lbl.Visible = true;
+                Reg_lbl.Visible = true;
+                MessageBox.Show("Uninstaller Test completed. \n RESULT : Some files/entries still present");
+                MessageBox.Show("Check the lists for tracked files/entries");
+                err_list.Visible = true;
+            } 
+        }
+
+        
+        
+    }
+
+    class installer_tests : filechk 
+    {
+       public string program_files(ListBox error_list,string[] progFiles_val, ListBox err_messages) //checks for existence of necessary files in Program Files
+        {
+           int flag = 0;
+           foreach (string val in progFiles_val)
+            {
+                if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\AdoMado\" + val))
+                {
+
+                    flag = 1;
+                    error_list.Items.Add(val);
+                    
+                }
+                               
+            }
+           if (flag == 1)
+           {
+               error_list.Items.Insert(0, "Following files were not found : ");
+               err_messages.Items.Add("Program files are missing");
+               return ("fail");
+           }
+           else
+           {
+               return ("Success");
+               
+           } 
+        }
+
+       public string registry_chk(ListBox list, string[] reg_val, Label regShow, ListBox err_messages) //checks for registry entries made by AdoMado(initiated by both installer and uninstaller)
+       {
+           RegistryKey reg1 = Registry.LocalMachine.OpenSubKey("SOFTWARE\\AdoMado\\AdoMado\\");
+           object regVal;
+           if(reg1!=null)
+           foreach (string val in reg_val)
+           {
+               regVal = reg1.GetValue(val);
+               regShow.Text += "\n\n" + val + " : " + regVal;
+           }
+          
+           if (reg1 == null)
+           {
+               err_messages.Items.Add("Registry entries returned null");
+               regShow.Text += "\n\nRegistry entries that returned null :\nSOFTWARE\\AdoMado\\AdoMado\\";
+               return ("Fail");
+           }
+           else
+           {
+               return ("Success");
+           }
+                
+       }
+
+       public string browser_chk(Label regShow, ListBox err_messages)
+       {
+           RegistryKey reg1 = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Mozilla\\Firefox\\Extensions\\");
+           RegistryKey reg2 = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Google\\Chrome\\Extensions\\");
+           RegistryKey reg3;
+           int flag = 0;
+           int count = 0;
+           object regVal_chrome;
+           object regVal_firefox;
+            regVal_firefox = reg1.GetValue("support@adomado.com");
+           if(regVal_firefox!=null)
+           {
+               regShow.Text += "\n\nFIREFOX : \nsupport@adomado.com : " + regVal_firefox;
+               count++;
+           }
+           if (regVal_firefox == null)
+           {
+               err_messages.Items.Add("Firefox extensions are empty");
+               regShow.Text+="\n\nFirefox Extensions returns null : \nSOFTWARE\\Mozilla\\Firefox\\Extensions\\";
+               
+           }
+           
+          foreach(string chromeExt in reg2.GetSubKeyNames())
+          {
+              reg3=Registry.LocalMachine.OpenSubKey("SOFTWARE\\Google\\Chrome\\Extensions\\" + chromeExt);
+              foreach(string extVal in reg3.GetValueNames())
+              {
+                regVal_chrome = reg3.GetValue(extVal);
+                   if (regVal_chrome.ToString() == (Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\AdoMado\\Chrome\\AdoMado.crx"))
+                   {
+                       regShow.Text += "\n\nCHROME : \nID : " +chromeExt + "\nPath : " + regVal_chrome;
+                       flag = 1;
+                       count++;
+                   }
+                   if (flag == 1)
+                   {
+                       regShow.Text += "\nVersion : " + reg3.GetValue("version").ToString();
+                       flag = 2;
+                       
+                   }
+
+
+               }
+           }
+               if (flag == 0)
+               {
+                   err_messages.Items.Add("Chrome extension is empty");
+                   regShow.Text+="\n\nChrome Extensions returns null :\nSOFTWARE\\Google\\Chrome\\Extensions\\<chromeID>\\";
+                   
+               }
+               if (count == 2)
+                   return ("Success");
+               else
+                   return ("Fail");
+       
+           
+       }
+
+       }
+
+    
+
+    class uninstaller_tests : filechk
+    {
+        public string program_files(ListBox error_list,ListBox err_message) //checks for exisitance of any remaining files in Program Files/AdoMado
+        {
+           int flag = 0;
+           
+          if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\AdoMado"))
+           {
+                flag = 1;
+                String[] subDirectories;
+                String[] subFiles;
+                subDirectories = System.IO.Directory.GetDirectories(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\AdoMado\");
+                subFiles = System.IO.Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\AdoMado\");
+                foreach (var subdir in subDirectories)
+                {
+                    error_list.Items.Add(subdir);
+                }
+                foreach (var subfil in subFiles)
+                {
+                    error_list.Items.Add(subfil);
+                }
+                    
+            }
+                
+          if (flag == 1)
+           {
+               error_list.Items.Insert(0, "Following Program files were not cleaned : ");
+               err_message.Items.Add("Program files entries still present");
+               return("Fail");
+           }
+           else
+           {
+               return("Success");
+           }
+
+           
+        }
+    }
+}
